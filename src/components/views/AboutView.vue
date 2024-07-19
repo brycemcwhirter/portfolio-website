64dbf5fc219499c7e46424784382b73819e10967
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-07-19 06:25:41 -0500

    Swapped card in blog from about.

--- src/components/views/AboutView.vue
+++ src/components/views/AboutView.vue
@@ -1,19 +1,12 @@
 <template>
   <div
     class="w-full h-16 flex items-center justify-center text-lg dark:bg-black dark:text-white"
-  >
-    <CardObject />
-  </div>
+  ></div>
 </template>
 
 <script>
-import CardObject from "../objects/CardObject.vue";
-
 export default {
   name: "AboutView",
-  components: {
-    CardObject,
-  },
 };
 </script>
 
--- src/components/views/BlogView.vue
+++ src/components/views/BlogView.vue
@@ -3,12 +3,18 @@
     class="w-full h-16 flex items-center justify-center text-lg dark:bg-black dark:text-white"
   >
     <h1>Blog</h1>
+    <CardObject />
   </div>
 </template>
 
 <script>
+import CardObject from "../objects/CardObject.vue";
+
 export default {
   name: "BlogView",
+  components: {
+    CardObject,
+  },
 };
 </script>
 
