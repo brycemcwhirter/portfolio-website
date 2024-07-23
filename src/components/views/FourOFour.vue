ed8f23aa19014a11856856167b534b9bf3338ec7
Bryce McWhirter <bryce.mcwhirter@paycomonline.com>
2024-07-23 12:05:03 -0500

    Four O Four

---
+++ src/components/views/FourOFour.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="w-full h-16 flex items-center justify-center text-lg dark:bg-black dark:text-white"
+  >
+    <h3>
+      Looks like you hit a 404. lol mah bad y'all. Just click on my name on the
+      top left corner
+    </h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FourOFour",
+};
+</script>
+
+<style scoped></style>
